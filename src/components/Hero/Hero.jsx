c7d4402e70cf82dfbd9dfd378bd69998a961b5a5
Alexmonsalve <alx120325@gmail.com>
2025-03-22 15:06:09 -0500

    Actualización de la página web logo pestaña

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -23,8 +23,7 @@ const Hero = () => {
           initial="initial"
           animate="animate"
           >  
-            "Lorem ipsum dolor sit amet, consectetur adipiscing 
-            elit, sed do eiusmod tempor incididunt ut labore et 
+            "LSOY MAKUAC empor incididunt ut labore et 
             dolore magna aliqua. Ut enim ad minim veniam, quis 
             nostrud exercitation ullamco laboris nisi ut aliquip 
             ex ea commodo consequat. Duis aute irure dolor in 
